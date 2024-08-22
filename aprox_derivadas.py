407fea7be2e36c0ce71ee03c479bcde5c92a873c
Manoel <manoelpiovesan@gmail.com>
2024-08-22 01:10:25 -0300

    Adding methods to approximate derivatives

---
+++ aprox_derivadas.py
@@ -0,0 +1,15 @@
+class AproxDerivadas:
+    
+    def __init__(self, f, h=1e-5):
+        self.f = f
+        self.h = h
+        
+    def df1(self, x):
+        return (self.f(x + self.h) - self.f(x)) / self.h
+    
+    def df2(self, x):
+        return (self.f(x + self.h) - self.f(x - self.h)) / (2 * self.h)
+    
+    def df3(self, x):
+        return (self.f(x - 2*self.h) - 8*self.f(x - self.h) + 8*self.f(x + self.h) - self.f(x + 2*self.h)) / (12 * self.h)
+    
--- met_newton.py
+++ met_newton.py
@@ -1,3 +1,5 @@
+from aprox_derivadas import AproxDerivadas as D
+
 # Função
 def f(x):
     return x**2 - 6*x + 5
@@ -26,14 +28,30 @@ def met_newton(f, df1, df2, x_inicial, tol, max_iter):
     print("Solução: ", x1)
     return x1
 
+def met_newton_derivadas_aprox(f, x_inicial, tol, max_iter):
+    i = 0
+    x0 = x_inicial
+    x1 = x0 - D(f).df1(x0) / D(f).df2(x0)
+    
+    while(abs(x1 - x0) > tol and i < max_iter):
+        x0 = x1
+        x1 = x0 - D(f).df1(x0) / D(f).df2(x0)
+        
+        print("Iteração: ", i, "x0: ", x0, "x1: ", x1, "f(x1): ", f(x1))
+        i+=1
+    
+    print("Solução: ", x1)
+    return x1
+    
 #
 # Implementação do método de Newton
 #
-a = 0
-b = 4
+a = 1
 tol = 0.0001
 max_iter = 100
 
-met_newton(f, df1, df2, a, tol, max_iter)
+# met_newton(f, df1, df2, a, tol, max_iter)
+
+met_newton_derivadas_aprox(f, a, tol, max_iter)
 
 
